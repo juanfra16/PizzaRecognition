886e614185d167aed95a00eab055eb6054bccd83
Esteban Villalobos <egvillalobos@uc.cl>
2020-07-12 14:35:06 -0400

    Se arregla un typo

--- detection/abstract.py
+++ detection/abstract.py
@@ -2,15 +2,15 @@
 Define clase abstracta para los detectores
 """
 
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
 import pandas as pd
 
 
 class AbstractPizzaDetector(ABC):
 
-    @abstractclassmethod
-    def detect_pizzas_in_video(self, video_path: str) -> pd.DataFame
-    """
+    @abstractmethod
+    def detect_pizzas_in_video(self, video_path: str) -> pd.DataFame:
+        """
         Método debe recibir el path a un video y como 
         output debe entregar un DataFrame con la siguiente estructura:
 
@@ -19,4 +19,6 @@ class AbstractPizzaDetector(ABC):
         Donde (x1, y1) es la esquina superior izquierda de la pizza
         y (x2, y2) es la esquina inferior izquierda
         """
-    pass
+        names = ["frame", "x1", "y1", "x2", "y2"]
+        example = pd.DataFrame(columns=names)
+        return example
